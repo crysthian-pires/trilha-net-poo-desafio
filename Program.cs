7f4935ec2f55cbd43e97797cb663bebc4eb207ef
crysthian-pires <crysthian.mello@gmail.com>
2025-07-07 18:19:15 -0300

    desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia c12 = new("5521484927", "C12", "354523972860632", 128);
+c12.Ligar();
+c12.InstalarAplicativo("WhatsApp");
+
+Iphone iphoneX = new("9539174965", "X", "336803088128923", 256);
+iphoneX.Ligar();
+iphoneX.InstalarAplicativo("WhatsApp");
